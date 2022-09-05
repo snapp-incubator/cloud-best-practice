8ed638b18d7184de757212d29a7e331301763525
Anis Hamidi <anis@MacBook-Pro-Anis.local>
2022-09-05 18:51:47 +0430

    update iaac/vm

--- iaac-instance/instances/network.tf
+++ iaac-instance/instances/network.tf
@@ -5,13 +5,9 @@ data "openstack_networking_subnet_v2" "private_subnet" {
   name = "private"
 }
 resource "openstack_networking_port_v2" "private" {
-  name                  = "${var.cluster_id}-${var.name}-private"
+  name                  = "${var.name}-private"
   admin_state_up        = "true"
   network_id            = data.openstack_networking_network_v2.private_network.id
   port_security_enabled = false
-  tags                  = ["openshiftClusterID=${var.cluster_id}"]
-  extra_dhcp_option {
-    name  = "domain-search"
-    value = var.cluster_domain
-  }
+  
 }
--- iaac-instance/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  region_domain  = "${var.region}.${var.base_domain}"
-  cluster_domain = "${var.cluster_id}.${var.region}.${var.base_domain}"
-}
